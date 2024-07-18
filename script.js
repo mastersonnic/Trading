8b8cdce9b68c3d36afa74748abcb9d75b14dd4b8
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-07-18 04:41:07 -0400

    Update script.js
    js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 // script.js
 function calcularCombinaciones() {
     const longitud = parseInt(document.getElementById('longitud-secuencia').value);
-    const combinaciones = calcularFactorial(longitud);
+    const combinaciones = calcularPermutaciones(longitud);
     document.getElementById('resultado').textContent = `Hay ${combinaciones} formas posibles de combinar las velas.`;
 }
 
-function calcularFactorial(n) {
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    let resultado = 1;
-    for (let i = 2; i <= n; i++) {
-        resultado *= i;
-    }
-    return resultado;
+function calcularPermutaciones(n) {
+    return Math.pow(2, n);
 }

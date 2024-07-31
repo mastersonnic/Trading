fbcd5b48521174171f67212d562b277b720b9831
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-07-31 19:24:04 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     const iframe = document.getElementById("webViewer");
-    iframe.src = "https://ff.io/";
+    iframe.src = "https://ff.io/rates/float.xml";
 });

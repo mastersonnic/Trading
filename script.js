cd5c2c44df7f28da45dc5575414e2e1d79c494ab
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-07-31 21:00:25 -0400

    Update script.js
    js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     const exchange1 = document.getElementById("exchange1");
     const exchange2 = document.getElementById("exchange2");
+    const iframe1 = document.getElementById("iframe1");
+    const iframe2 = document.getElementById("iframe2");
     const link1 = document.getElementById("link1");
     const link2 = document.getElementById("link2");
 
-    function updateLinks() {
+    function updateIframes() {
+        iframe1.src = exchange1.value;
+        iframe2.src = exchange2.value;
         link1.href = exchange1.value;
         link1.textContent = `Abrir ${exchange1.options[exchange1.selectedIndex].text}`;
         link2.href = exchange2.value;
         link2.textContent = `Abrir ${exchange2.options[exchange2.selectedIndex].text}`;
     }
 
-    exchange1.addEventListener("change", updateLinks);
-    exchange2.addEventListener("change", updateLinks);
+    exchange1.addEventListener("change", updateIframes);
+    exchange2.addEventListener("change", updateIframes);
 
-    // Inicializar enlaces con los valores por defecto
-    updateLinks();
+    // Inicializar iframes con los valores por defecto
+    updateIframes();
 });

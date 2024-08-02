437a617cbfe868e259a12310fb894de307e6ea8c
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-01 20:52:56 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // script.js
 fetch('https://ff.io/rates/float.xml')
-  .then(response => response.text())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al cargar el XML');
+    }
+    return response.text();
+  })
   .then(data => {
-    document.getElementById('xml-content').textContent = data;
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(data, "application/xml");
+    const serializer = new XMLSerializer();
+    const xmlStr = serializer.serializeToString(xmlDoc);
+    document.getElementById('xml-content').textContent = xmlStr;
   })
   .catch(error => console.error('Error al cargar el XML:', error));

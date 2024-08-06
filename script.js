5de1f913f4edb225954fec867cdd76a0ffed8f91
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-06 12:39:08 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -22,8 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const frente = document.getElementById('frente');
     let activeVisor = null;
 
-        const fichas = ['0-0', '0-1', '0-2', '0-3', '0-4', '0-5', '0-6', '1-1', '1-2', '1-3', '1-4', '1-5', '1-6', '2-2', '2-3', '2-4', '2-5', '2-6', '3-3', '3-4', '3-5', '3-6', '4-4', '4-5', '4-6', '5-5', '5-6', '6-6'];
-    fichas.forEach(ficha => {
+        const fichas = ['0-0', '0-1', '0-2', '0-3', '0-4', '0-5', '0-6', '1-1', '1-2', '1-3', '1-4', '1-5', '1-6', '2-2', '2-3', '2-4', '2-5', '2-6', '3-3', '3-4', '3-5', '3-6', '4-4', '4-5', '4-6', '5-5', '5-6', '6-6']; 
     // Funcionalidad al hacer clic en una ficha
     fichas.forEach(ficha => {
         const fichaDiv = document.createElement('div');

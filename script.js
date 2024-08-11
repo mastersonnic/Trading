5949c5f05c770903f221e32d319f771fb09da286
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-10 22:05:39 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -18,10 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const acabarManoButton = document.getElementById('acabar-mano');
     const grupoFichas = document.getElementById('grupo-fichas');
     let activeJugadas = 'yo';
-    let fichaSeleccionada = null; // Variable para almacenar la ficha seleccionada
+    let fichaSeleccionada = null;
 
     const fichas = ['0-0', '0-1', '0-2', '0-3', '0-4', '0-5', '0-6', '1-1', '1-2', '1-3', '1-4', '1-5', '1-6', '2-2', '2-3', '2-4', '2-5', '2-6', '3-3', '3-4', '3-5', '3-6', '4-4', '4-5', '4-6', '5-5', '5-6', '6-6'];
-
     fichas.forEach(ficha => {
         const fichaDiv = document.createElement('div');
         fichaDiv.className = 'ficha';
@@ -29,11 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
         fichaDiv.addEventListener('click', () => {
             fichaSeleccionada = ficha;
 
-            // Mostrar la ficha seleccionada junto al marco del jugador "Yo"
             const fichaMostrada = document.createElement('div');
             fichaMostrada.textContent = ficha;
-            jugadasSelects.yo.appendChild(fichaMostrada);
-            
+
             const jugadasDiv = jugadasSelects[activeJugadas];
             const paseDiv = jugadasSelects[`pase${activeJugadas.charAt(0).toUpperCase() + activeJugadas.slice(1)}`];
             const salioDiv = jugadasSelects[`salio${activeJugadas.charAt(0).toUpperCase() + activeJugadas.slice(1)}`];
@@ -77,4 +74,4 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-});  
+});

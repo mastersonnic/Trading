b65bb5e75a51cc64299adbb569514a2740bceb3a
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-13 16:45:21 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ function selectCell(cell) {
 }
 
 function copyToClipboard(element) {
-    const text = element.innerText;
+    const text = element.innerText.replace('|', ',');
     navigator.clipboard.writeText(text).then(() => {
         showConfirmation();
         if (autoPaste && selectedCell) {
             const currentText = selectedCell.innerText;
-            selectedCell.innerText = currentText ? currentText + ', ' + text : text;
+            if (selectedCell.id === 'B3' && element.classList.contains('domino') && element.innerText.includes('0, 0')) {
+                const targetCell = document.querySelector('#B2');
+                const targetText = targetCell.innerText;
+                targetCell.innerText = targetText ? targetText + ', ' + text : text;
+            } else {
+                selectedCell.innerText = currentText ? currentText + ', ' + text : text;
+            }
         }
     }).catch(err => {
         console.error('Error al copiar al portapapeles: ', err);

c39a877182156257a30504fb35779c2702473b3a
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-13 15:22:52 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function copyToClipboard(element) {
     const text = element.innerText;
-    navigator.clipboard.writeText(text).catch(err => {
+    navigator.clipboard.writeText(text).then(() => {
+        showConfirmation();
+    }).catch(err => {
         console.error('Error al copiar al portapapeles: ', err);
     });
+}
+
+function showConfirmation() {
+    const confirmation = document.getElementById('confirmation');
+    confirmation.style.opacity = '1';
+    setTimeout(() => {
+        confirmation.style.opacity = '0';
+    }, 500);
 }

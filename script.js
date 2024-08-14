06a215666be2c5940da614fb97d3de6396e55c79
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-08-14 11:32:57 -0400

    Update script.js
    Js

--- script.js
+++ script.js
@@ -38,14 +38,7 @@ function copyToClipboard(element) {
         showConfirmation();
         if (autoPaste && selectedCell) {
             const currentText = selectedCell.innerText;
-            const isDouble = element.matches('.domino:nth-child(1), .domino:nth-child(8), .domino:nth-child(14), .domino:nth-child(19), .domino:nth-child(23), .domino:nth-child(26), .domino:nth-child(28)');
-            if (selectedCell.id === 'B3' && isDouble) {
-                const targetCell = document.querySelector('#B2');
-                const targetText = targetCell.innerText;
-                targetCell.innerText = targetText ? targetText + ', ' + text : text;
-            } else {
-                selectedCell.innerText = currentText ? currentText + ', ' + text : text;
-            }
+            selectedCell.innerText = currentText ? currentText + ', ' + text : text;
             saveState();
         }
     }).catch(err => {

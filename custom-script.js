8cae947b193ca9e5b261f67f0642f07f9f0ce584
mastersonnic <68176271+mastersonnic@users.noreply.github.com>
2024-09-04 16:49:44 -0400

    Update custom-script.js
    Js

--- custom-script.js
+++ custom-script.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const visorJ1 = document.getElementById('mis-fichas');
 
+    function actualizarVisorJ1() {
+        const fichasSeleccionadas = document.querySelectorAll('#fichas input[type="checkbox"]:checked');
+        visorJ1.innerHTML = ''; 
+        fichasSeleccionadas.forEach(checkbox => {
+            const fichaLabel = checkbox.nextSibling.textContent;
+            visorJ1.innerHTML += `<div>${fichaLabel}</div>`;
+        });
+    }
+
+    const checkboxes = document.querySelectorAll('#fichas input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', actualizarVisorJ1);
+    });
+
+    actualizarVisorJ1(); // Inicializa el visor
+});
